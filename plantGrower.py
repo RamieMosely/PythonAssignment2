cb540a3f5779d574514b34c5c9c770e3910c80c2
Ramie Mosely <52608883+RamieMosely@users.noreply.github.com>
2024-10-29 20:07:08 -0400

    finishedFunctions

--- plantGrower.py
+++ plantGrower.py
@@ -49,3 +49,45 @@ def compareHeights(plant1, plant2):
 
     print(f"{plant1.name}: {height1:.2f} cm")
     print(f"{plant2.name}: {height2:.2f} cm")
+
+def findOvertakeDay(plant1, plant2):
+    if plant1.growthRate <= plant2.growthRate and plant1.height >= plant2.height:
+        print(f"{plant1.name} starts taller and grows quicker or at the same rate as {plant2.name}.")
+        print("There wont be any overtaking in growth!")
+        return
+    elif plant2.growthRate <= plant1.growthRate and plant2.height >= plant1.height:
+        print(f"{plant2.name} starts taller and grows quicker or at the same rate as {plant1.name}.")
+        print("There wont be any overtaking in growth!")
+        return
+    
+    #overtake
+    if plant1.growthRate > plant2.growthRate:
+        faster = plant1
+        slower = plant2
+    else:
+        faster = plant2
+        slower = plant1
+
+    days = (slower.height - faster.height) / (faster.growthRate - slower.growthRate)
+    if days < 0:
+        print(f"{faster.name} is taller than {slower.name}")
+    else:
+        days = int(days) + 1
+        print(f"{faster.name} will be taller than {slower.name} on day {days}")
+        print(f"Height of {faster.name}: {faster.getHeightAfterDays(days):.2f} cm")
+        print(f"Height of {slower.name}: {slower.getHeightAfterDays(days):.2f} cm")
+
+def changePlantInfo(plant1, plant2):
+    while True:
+        plantNum = input("Which plant would you like to modify?(1 or 2)\n")
+        if plantNum == '1' or plantNum == '2':
+            break
+        print("Please enter either 1 or 2")
+
+    name, height, growthRate = getPlantInfo()
+    if plantNum == '1':
+        return Plant(name, height, growthRate), plant2
+    
+    else:
+        return plant1, Plant(name, height, growthRate)
+
